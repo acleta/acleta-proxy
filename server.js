2e12a5734c39f0d4d1f329a48d63dde598077425
Seba Gamboa <me@sagmor.com>
2015-03-06 00:19:10 -0300

    Use dynamic port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('./app/application');
+var port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
-  console.log('Proxy listening at http://0.0.0.0:%s', 3000);
+app.listen(port, function () {
+  console.log('Proxy listening at http://0.0.0.0:%s', port);
 })
