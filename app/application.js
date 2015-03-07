48b624ae0bf1cbb518302aa60ef02d1e05db50af
Seba Gamboa <me@sagmor.com>
2015-03-07 12:00:28 -0300

    Updated URLs

--- app/application.js
+++ app/application.js
@@ -2,8 +2,8 @@ var express = require('express')
 
 var app = express();
 
-app.get('/bike-santiago.json', require('./handlers/bike_santiago_proxy') );
-app.get('/bike-santiago.geojson', require('./handlers/bike_santiago_converter') );
-app.get('/*', require('./handlers/dataset_proxy') );
+app.get('/sets/services/bike-santiago.json', require('./handlers/bike_santiago_proxy') );
+app.get('/sets/services/bike-santiago.geojson', require('./handlers/bike_santiago_converter') );
+app.get('/sets/*', require('./handlers/dataset_proxy') );
 
 module.exports = app;
